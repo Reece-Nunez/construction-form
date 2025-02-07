ead9335fcf5f961f66b8b42f7e0ac7bfc592b91b
Reece Nunez <116838102+Reece-Nunez@users.noreply.github.com>
2025-02-07 06:13:30 +0000

    having issues with cors and not finding the correct url for posting

--- src/components/ConstructionProjectForm.js
+++ src/components/ConstructionProjectForm.js
@@ -82,7 +82,7 @@ const ConstructionProjectForm = () => {
     setShowPreviewModal(false);
 
     try {
-      const response = await axios.post("https://yqa9atlz7f.execute-api.us-east-1.amazonaws.com/production", formData);
+      const response = await axios.post("https://yqa9atlz7f.execute-api.us-east-1.amazonaws.com/production/submit", formData);
       console.log("API response: ", response.data);
       setShowSuccessModal(true);
       setFormData({
